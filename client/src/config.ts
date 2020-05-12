9896079d3f66c0fe4dd3dd8493e42b6c60799969
Rafael Ortega <rafao1989@hotmail.com>
2020-05-13 00:10:08 +0200

    feat: configure client to be able to authorize

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'er4xsl3j4j'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'rafao.auth0.com',
+  clientId: 'ul8aYAZYVHegBplQTo9iCc2LF2Ra2ZSu',
   callbackUrl: 'http://localhost:3000/callback'
 }
